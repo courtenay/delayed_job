12c91772d9b2b1a34c18f964125e4bd1d3cb1dd1
Jonathan del Strother <jon.delStrother@audioboo.fm>
2011-09-06 09:51:56 +0100

    Use actual newlines in job.last_error

--- lib/delayed/worker.rb
+++ lib/delayed/worker.rb
@@ -167,8 +167,8 @@ module Delayed
       end
       say "#{job.name} completed after %.4f" % runtime
       return true  # did work
-    rescue ::Delayed::DeserializationError => error
-      job.last_error = "{#{error.message}\n#{error.backtrace.join('\n')}"
+    rescue DeserializationError => error
+      job.last_error = "{#{error.message}\n#{error.backtrace.join("\n")}"
       failed(job)
     rescue Exception => error
       self.class.lifecycle.run_callbacks(:error, self, job){ handle_failed_job(job, error) }
@@ -209,7 +209,7 @@ module Delayed
   protected
 
     def handle_failed_job(job, error)
-      job.last_error = "{#{error.message}\n#{error.backtrace.join('\n')}"
+      job.last_error = "{#{error.message}\n#{error.backtrace.join("\n")}"
       say "#{job.name} failed with #{error.class.name}: #{error.message} - #{job.attempts} failed attempts", Logger::ERROR
       reschedule(job)
     end
